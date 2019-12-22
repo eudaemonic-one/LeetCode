fa3a5466027771b09e6678468c28613629f8e903
Rong Cui <brookcui97@gmail.com>
2019-12-22 15:13:28 +0800

    [Google] 734. Sentence Similarity (Golang+)
    https://leetcode.com/problems/sentence-similarity/

--- Google/Golang/sentence-similarity.go
+++ Google/Golang/sentence-similarity.go
@@ -2,34 +2,17 @@ func areSentencesSimilar(words1 []string, words2 []string, pairs [][]string) boo
     if len(words1) != len(words2) {
         return false
     }
-    dict := make(map[string][]string)
+    pairset := make(map[[2]string]bool)
     for _, pair := range pairs {
         w1, w2 := pair[0], pair[1]
-        dict[w1] = append(dict[w1], w2)
-        dict[w2] = append(dict[w2], w1)
+        pairset[[2]string{w1,w2}] = true
     }
     for i := 0; i < len(words2); i++ {
         w1, w2 := words1[i], words2[i]
-        if w1 != w2 {
-            flag := false
-            for _, w := range dict[w1] {
-                if w == w2 {
-                    flag = true
-                    break
-                }
-            }
-            if flag {
-                continue
-            }
-            for _, w := range dict[w2] {
-                if w == w1 {
-                    flag = true
-                    break
-                }
-            }
-            if !flag {
-                return false
-            }
+        _, ok1 := pairset[[2]string{w1,w2}]
+        _, ok2 := pairset[[2]string{w2,w1}]
+        if w1 != w2 && !ok1 && !ok2 {
+            return false
         }
     }
     return true
