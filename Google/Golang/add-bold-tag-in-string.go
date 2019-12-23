1ed240806feced86f3130ce534f06f13bb68453f
Rong Cui <brookcui97@gmail.com>
2019-12-23 14:24:43 +0800

    [Google] 616. Add Bold Tag in String (Golang+)
    https://leetcode.com/problems/add-bold-tag-in-string/

--- Google/Golang/add-bold-tag-in-string.go
+++ Google/Golang/add-bold-tag-in-string.go
@@ -1,30 +1,43 @@
+type Intervals [][2]int
+
+func (intervals Intervals) Len() int {
+    return len(intervals)
+}
+
+func (intervals Intervals) Less(i, j int) bool {
+    if intervals[i][0] == intervals[j][0] {
+        return intervals[i][1] < intervals[j][1]
+    } else {
+        return intervals[i][0] < intervals[j][0]
+    }
+}
+
+func (intervals Intervals) Swap(i, j int) {
+    intervals[i], intervals[j] = intervals[j], intervals[i]
+}
+
 func addBoldTag(s string, dict []string) string {
-    wrapIndex := make([]int, 0)
+    intervals := Intervals{}
     res := make([]byte, 0)
     for i := 0; i < len(s); i++ {
-        maxRange := 0
         for _, substr := range dict {
-            if strings.Index(s[i:], substr) == 0 && len(substr) > maxRange {
-                maxRange = len(substr)
-                start, end := i, i+len(substr)
-                if len(wrapIndex) == 0 || start > wrapIndex[len(wrapIndex)-1] {
-                    wrapIndex = append(wrapIndex, start)
-                    wrapIndex = append(wrapIndex, end)
-                }
-                if start <= wrapIndex[len(wrapIndex)-1] {
-                    if start < wrapIndex[len(wrapIndex)-2] {
-                        wrapIndex[len(wrapIndex)-2] = start
-                    }
-                    if end > wrapIndex[len(wrapIndex)-1] {
-                        wrapIndex[len(wrapIndex)-1] = end
-                    }
-                }
+            if strings.Index(s[i:], substr) == 0 {
+                intervals = append(intervals, [2]int{i,i+len(substr)})
             }
         }
     }
+    sort.Sort(intervals)
+    merged := Intervals{}
+    for _, interval := range intervals {
+        if len(merged) == 0 || merged[len(merged)-1][1] < interval[0] {
+            merged = append(merged, interval)
+        } else {
+            merged[len(merged)-1][1] = max(merged[len(merged)-1][1], interval[1])
+        }
+    }
     last := 0
-    for i := 0; i < len(wrapIndex); i += 2 {
-        start, end := wrapIndex[i], wrapIndex[i+1]
+    for _, interval := range merged {
+        start, end := interval[0], interval[1]
         res = append(res, s[last:start]...)
         res = append(res, "<b>"...)
         res = append(res, s[start:end]...)
@@ -35,4 +48,11 @@ func addBoldTag(s string, dict []string) string {
         res = append(res, s[last:]...)
     }
     return string(res)
+}
+
+func max(x, y int) int {
+    if x > y {
+        return x
+    }
+    return y
 }
