b0f05c0af89586e3f721966bceb86621a05c6f35
Rong Cui <brookcui97@gmail.com>
2019-12-29 16:56:38 -0600

    [Google] 32. Longest Valid Parentheses (Golang++)
    https://leetcode.com/problems/longest-valid-parentheses/

--- Google/Golang/longest-valid-parentheses.go
+++ Google/Golang/longest-valid-parentheses.go
@@ -1,16 +1,29 @@
 func longestValidParentheses(s string) int {
     maxans := 0
-    stack := []int{-1}
+    left, right := 0, 0
     for i := 0; i < len(s); i++ {
         if s[i] == '(' {
-            stack = append(stack, i)
+            left++
         } else {
-            stack = stack[:len(stack)-1]
-            if len(stack) == 0 {
-                stack = append(stack, i)
-            } else {
-                maxans = max(maxans, i-stack[len(stack)-1])
-            }
+            right++
+        }
+        if left == right {
+            maxans = max(maxans, left+right)
+        } else if right > left {
+            left, right = 0, 0
+        }
+    }
+    left, right = 0, 0
+    for i := len(s)-1; i >= 0; i-- {
+        if s[i] == '(' {
+            left++
+        } else {
+            right++
+        }
+        if left == right {
+            maxans = max(maxans, left+right)
+        } else if left > right {
+            left, right = 0, 0
         }
     }
     return maxans
