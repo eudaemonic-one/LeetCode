08181c4cc0c7e504abe7896990822c2768037fbe
Rong Cui <brookcui97@gmail.com>
2020-01-05 12:43:36 -0500

    [Google] 459. Repeated Substring Pattern (Golang+)
    https://leetcode.com/problems/repeated-substring-pattern/

--- Google/Golang/repeated-substring-pattern.go
+++ Google/Golang/repeated-substring-pattern.go
@@ -1,15 +1,6 @@
 func repeatedSubstringPattern(s string) bool {
-    for i := 0; i < len(s)-1; i++ {
-        if len(s) % (i+1) != 0 {
-            continue
-        }
-        substr, t := s[:i+1], ""
-        for j := 0; j < (len(s)/(i+1)); j++ {
-            t += substr
-        }
-        if s == t {
-            return true
-        }
+    if len(s) <= 1 {
+        return false
     }
-    return false
+    return strings.Index((s+s)[1:len(s)*2-1], s) != -1
 }
