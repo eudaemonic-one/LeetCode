149dffd380ba403fc0d402510bd1512a1d472070
Rong Cui <brookcui97@gmail.com>
2020-01-05 21:34:48 -0500

    [Google] 482. License Key Formatting (Golang+)
    https://leetcode.com/problems/license-key-formatting/

--- Google/Golang/license-key-formattin.go
+++ Google/Golang/license-key-formattin.go
@@ -1,24 +1,12 @@
 func licenseKeyFormatting(S string, K int) string {
-    withoutDashes := make([]byte, 0)
-    for i := 0; i < len(S); i++ {
-        if S[i] != '-' {
-            withoutDashes = append(withoutDashes, upper(S[i]))
-        }
-    }
-    j := len(withoutDashes)
     res := make([]byte, 0)
-    for j-K > 0 {
-        if j != len(withoutDashes) {
-            res = append([]byte{'-'}, res...)
+    for i :=  len(S)-1; i >= 0; i-- {
+        if S[i] != '-' {
+            if len(res)%(K+1) == K {
+                res = append([]byte{'-'}, res...)
+            }
+            res = append([]byte{upper(S[i])}, res...)
         }
-        res = append(withoutDashes[j-K:j], res...)
-        j -= K
-    }
-    if j > 0 {
-        if j != len(withoutDashes) {
-            res = append([]byte{'-'}, res...)
-        }
-        res = append(withoutDashes[:j], res...)
     }
     return string(res)
 }
