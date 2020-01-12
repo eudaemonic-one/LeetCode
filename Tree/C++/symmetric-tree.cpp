7155f5254f03dddec7e912ee0f1a6ca580624e20
Rong Cui <brookcui97@gmail.com>
2020-01-11 21:19:51 -0500

    [Tree] 101. Symmetric Tree (CPP+)
    https://leetcode.com/problems/symmetric-tree/

--- Tree/C++/symmetric-tree.cpp
+++ Tree/C++/symmetric-tree.cpp
@@ -10,16 +10,25 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        return isMirror(root, root);
-    }
-
-    bool isMirror(TreeNode* t1, TreeNode* t2) {
-        if (t1 == NULL && t2 == NULL) {
-            return true;
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(root);
+        while (!q.empty()) {
+            TreeNode *t1 = q.front();
+            q.pop();
+            TreeNode *t2 = q.front();
+            q.pop();
+            if (t1 == NULL && t2 == NULL) {
+                continue;
+            }
+            if (t1 == NULL || t2 == NULL || t1->val != t2->val) {
+                return false;
+            }
+            q.push(t1->left);
+            q.push(t2->right);
+            q.push(t1->right);
+            q.push(t2->left);
         }
-        if (t1 == NULL || t2 == NULL) {
-            return false;
-        }
-        return (t1->val == t2->val && isMirror(t1->right, t2->left) && isMirror(t1->left, t2->right));
+        return true;
     }
 };
