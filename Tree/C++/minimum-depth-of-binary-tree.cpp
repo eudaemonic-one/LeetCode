12e41cf1ef36ea38aa620650fce2340b7f6d3291
Rong Cui <brookcui97@gmail.com>
2020-01-14 09:46:38 -0500

    [Tree] 111. Minimum Depth of Binary Tree (CPP++)
    https://leetcode.com/problems/minimum-depth-of-binary-tree/

--- Tree/C++/minimum-depth-of-binary-tree.cpp
+++ Tree/C++/minimum-depth-of-binary-tree.cpp
@@ -13,27 +13,27 @@ public:
         if (root == NULL) {
             return 0;
         }
-        if (root->left == NULL && root->right == NULL) {
-            return 1;
+        queue<TreeNode*> q;
+        q.push(root);
+        TreeNode * node;
+        int depth = 0;
+        while (!q.empty()) {
+            int n = q.size();
+            ++depth;
+            for (int i = 0; i < n; i++) {
+                node = q.front();
+                q.pop();
+                if (node->left == NULL && node->right == NULL) {
+                    return depth;
+                }
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
         }
-        int min_depth = INT_MAX;
-        stack<pair<TreeNode*, int>> stack;
-        stack.push(pair<TreeNode*, int>(root, 1));
-        while (!stack.empty()) {
-            pair<TreeNode*, int> curr = stack.top();
-            stack.pop();
-            TreeNode* node = curr.first;
-            int depth = curr.second;
-            if (node->left == NULL && node->right == NULL) {
-                min_depth = min(min_depth, depth);
-            }
-            if (node->left) {
-                stack.push(pair<TreeNode*, int>(node->left, depth+1));
-            }
-            if (node->right) {
-                stack.push(pair<TreeNode*, int>(node->right, depth+1));
-            }
-        }
-        return min_depth;
+        return depth;
     }
 };
