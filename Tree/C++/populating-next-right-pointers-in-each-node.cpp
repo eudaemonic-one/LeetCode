0c7093ce963868790258bf63b9fdf233173b9305
Rong Cui <brookcui97@gmail.com>
2020-01-15 10:49:34 -0500

    [Tree] 116. Populating Next Right Pointers in Each Node (CPP)
    https://leetcode.com/problems/populating-next-right-pointers-in-each-node/

---
+++ Tree/C++/populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,48 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if (root == NULL) {
+            return NULL;
+        }
+        Node *node;
+        int n = 0;
+        queue<Node*> q;
+        q.push(root);
+        while (!q.empty()) {
+            n = q.size();
+            for (int i = 0; i < n; i++) {
+                node = q.front();
+                q.pop();
+                if (i < n-1) {
+                    node->next = q.front();
+                }
+                if (node->left) {
+                    q.push(node->left);
+                }
+                if (node->right) {
+                    q.push(node->right);
+                }
+            }
+        }
+        return root;
+    }
+};
