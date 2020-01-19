f1177c2af247316c382ee9c9c5b1bd16cb1754f4
Rong Cui <brookcui97@gmail.com>
2020-01-19 14:22:45 -0500

    [Tree] 222. Count Complete Tree Nodes (CPP+)
    https://leetcode.com/problems/count-complete-tree-nodes/

--- Tree/C++/count-complete-tree-nodes.cpp
+++ Tree/C++/count-complete-tree-nodes.cpp
@@ -1,4 +1,4 @@
-o/**
+/**
  * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
@@ -10,23 +10,39 @@ o/**
 class Solution {
 public:
     int countNodes(TreeNode* root) {
-        int count = 0;
-        TreeNode *node;
-        stack<TreeNode*> s;
-        s.push(root);
-        while (!s.empty()) {
-            node = s.top();
-            s.pop();
-            if (node) {
-                ++count;
-                if (node->left) {
-                    s.push(node->left);
-                }
-                if (node->right) {
-                    s.push(node->right);
-                }
+        int depth = 0, left, pivot, right;
+        TreeNode *node = root;
+        if (root == NULL)
+            return 0;
+        while (node && node->left) {
+            node = node->left;
+            ++depth;
+        }
+        if (depth == 0)
+            return 1;
+        left = 1, right = pow(2, depth)-1;
+        while (left <= right) {
+            pivot = floor((left + right) / 2);
+            if (exists(pivot, depth, root))
+                left = pivot + 1;
+            else
+                right = pivot - 1;
+        }
+        return int(pow(2, depth)) - 1 + left;
+    }
+    
+    bool exists(int idx, int depth, TreeNode *root) {
+        int left = 0, right = int(pow(2, depth)) - 1, pivot;
+        for (int i = 0; i < depth; i++) {
+            pivot = floor((left + right) / 2);
+            if (idx <= pivot) {
+                root = root->left;
+                right = pivot;
+            } else {
+                root = root->right;
+                left = pivot + 1;
             }
         }
-        return count;
+        return root != NULL;
     }
 };
