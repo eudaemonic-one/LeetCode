59c16684af82b4927e08fe4e9b117235d04cc22f
Rong Cui <brookcui97@gmail.com>
2020-05-20 22:33:02 -0400

    [Google] 31. Next Permutation (Golang+)
    https://leetcode.com/problems/next-permutation/

--- Google/Golang/next-permutation.go
+++ Google/Golang/next-permutation.go
@@ -1,16 +1,21 @@
 func nextPermutation(nums []int)  {
-    i := len(nums)-2
-    for i >= 0 && nums[i+1] <= nums[i] {
+    i := len(nums) - 1
+    for i > 0 && nums[i] <= nums[i-1] {
         i--
     }
-    if i >= 0 {
-        j := len(nums)-1
-        for j >= 0 && nums[j] <= nums[i] {
+    
+    if i - 1 >= 0 {
+        j := len(nums) - 1
+        for j > 0 && nums[j] <= nums[i-1] {
             j--
         }
-        nums[i], nums[j] = nums[j], nums[i]
+        nums[i-1], nums[j] = nums[j], nums[i-1]
     }
-    for l, r := i+1, len(nums)-1; l < r; l, r = l+1, r-1 {
+    
+    l, r := i, len(nums) - 1
+    for l < r {
         nums[l], nums[r] = nums[r], nums[l]
+        l++
+        r--
     }
 }
