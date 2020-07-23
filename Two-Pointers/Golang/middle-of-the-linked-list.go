e9c0c32547a67e80eaf9d87995793887e1ca66c4
Rong Cui <brookcui97@gmail.com>
2020-07-22 21:25:52 -0400

    [Two-Pointers] 876. Middle of the Linked List (Golang)
    https://leetcode.com/problems/middle-of-the-linked-list/

---
+++ Two-Pointers/Golang/middle-of-the-linked-list.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func middleNode(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
+    }
+    slow, fast := head, head
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+    return slow
+}
