93492af4346090b9052584a67fa836314327215c
Rong Cui <brookcui97@gmail.com>
2020-08-10 14:18:02 -0400

    [Amazon] 23. Merge k Sorted Lists (Golang)
    https://leetcode.com/problems/merge-k-sorted-lists/

---
+++ Amazon/Golang/merge-k-sorted-lists.go
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func mergeKLists(lists []*ListNode) *ListNode {
+    if len(lists) == 0 {
+        return nil
+    }
+    for interval := 1; interval < len(lists); interval *= 2 {
+        for i := 0; i < len(lists)-interval; i += interval * 2 {
+            lists[i] = merge2Lists(lists[i], lists[i+interval])
+        }
+    }
+    return lists[0]
+}
+
+func merge2Lists(l1, l2 *ListNode) *ListNode {
+    dummy := &ListNode{}
+    curr := dummy
+    for l1 != nil && l2 != nil {
+        if l1.Val <= l2.Val {
+            curr.Next = l1
+            l1 = l1.Next
+        } else {
+            curr.Next = l2
+            l2 = l2.Next
+        }
+        curr = curr.Next
+    }
+    if l1 != nil {
+        curr.Next = l1
+    } else {
+        curr.Next = l2
+    }
+    return dummy.Next
+}
+
