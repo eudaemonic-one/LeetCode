40d51b85d39776c9f64c5417f813ed4e52c6e963
Rong Cui <brookcui97@gmail.com>
2021-01-06 01:45:11 -0500

    [Java] 64. Minimum Path Sum (Optimized)
    https://leetcode.com/problems/minimum-path-sum/

--- Java/64.java
+++ Java/64.java
@@ -1,21 +1,45 @@
+// Approach 2: Dynamic Programming 1D
 class Solution {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int dp[][] = new int[m][n];
+        int dp[] = new int[n];
         for (int i = m-1; i >= 0; i--) {
             for (int j = n-1; j >= 0; j--) {
                 if (i == m-1 && j != n-1) {
-                    dp[i][j] = grid[i][j] + dp[i][j+1];
+                    dp[j] = grid[i][j] + dp[j+1];
                 } else if (j == n-1 && i != m-1) {
-                    dp[i][j] = grid[i][j] + dp[i+1][j];
+                    dp[j] = grid[i][j] + dp[j];
                 } else if (j != n-1 && i != m-1) {
-                    dp[i][j] = grid[i][j] + Math.min(dp[i+1][j], dp[i][j+1]);
+                    dp[j] = grid[i][j] + Math.min(dp[j], dp[j+1]);
                 } else {
-                    dp[i][j] = grid[i][j];
+                    dp[j] = grid[i][j];
                 }
             }
         }
-        return dp[0][0];
+        return dp[0];
     }
 }
+
+// Approach 1: Dynamic Programming 2D
+// class Solution {
+//     public int minPathSum(int[][] grid) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+//         int dp[][] = new int[m][n];
+//         for (int i = m-1; i >= 0; i--) {
+//             for (int j = n-1; j >= 0; j--) {
+//                 if (i == m-1 && j != n-1) {
+//                     dp[i][j] = grid[i][j] + dp[i][j+1];
+//                 } else if (j == n-1 && i != m-1) {
+//                     dp[i][j] = grid[i][j] + dp[i+1][j];
+//                 } else if (j != n-1 && i != m-1) {
+//                     dp[i][j] = grid[i][j] + Math.min(dp[i+1][j], dp[i][j+1]);
+//                 } else {
+//                     dp[i][j] = grid[i][j];
+//                 }
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// }
