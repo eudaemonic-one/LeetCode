caef620e589e8a4a5bec5b8cac596c16d2fcc74b
Rong Cui <brookcui97@gmail.com>
2021-02-20 17:46:31 -0500

    [Google] 743. Network Delay Time
    Link: https://leetcode.com/problems/network-delay-time/
    
    Topics: Heap, Depth-first Search, Breadth-first Search, Graph
    
    Approach: Dijkstra's Algorithm
    
    Time Complexity: O(ElogE)
    
    Space Complexity: O(N + E)
    
    Review:
    1. We use Dijkstra's algorithm to find the shortest path from one source to all targets.
    2. Dijkstra's algorithm is based on repeatedly making the candidate move that has the least distance travelled.

--- Google/743.java
+++ Google/743.java
@@ -1,3 +1,5 @@
+// Approach #2: Dijkstra's Algorithm
+
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
         Map<Integer, List<int[]>> graph = new HashMap<>();
@@ -7,36 +9,80 @@ class Solution {
             }
             graph.get(edge[0]).add(new int[]{edge[1], edge[2]});
         }
-        for (int node: graph.keySet()) {
-            Collections.sort(graph.get(node), (a, b) -> a[1] - b[1]);
-        }
         
+        PriorityQueue<int[]> heap = new PriorityQueue<>((pair1, pair2) -> pair1[1] - pair2[1]);
+        heap.offer(new int[]{k, 0});
         Map<Integer, Integer> distMap = new HashMap<>();
-        for (int node = 1; node <= n; node++) {
-            distMap.put(node, Integer.MAX_VALUE);
+        
+        while (!heap.isEmpty()) {
+            int[] pair = heap.poll();
+            int node = pair[0], elapsed = pair[1];
+            if (distMap.containsKey(node)) {
+                continue;
+            }
+            distMap.put(node, elapsed);
+            if (graph.containsKey(node)) {
+                for (int[] edge : graph.get(node)) {
+                    int nei = edge[0], dist = edge[1];
+                    if (!distMap.containsKey(nei)) {
+                        heap.offer(new int[]{nei, elapsed+dist});
+                    }
+                }
+            }
         }
         
-        dfs(graph, distMap, k, 0);
-        
+        if (distMap.size() != n) {
+            return -1;
+        }
         int res = 0;
         for (int cand : distMap.values()) {
-            if (cand == Integer.MAX_VALUE) {
-                return -1;
-            }
             res = Math.max(res, cand);
         }
         return res;
     }
-    
-    private void dfs(Map<Integer, List<int[]>> graph, Map<Integer, Integer> distMap, int node, int elapsed) {
-        if (elapsed >= distMap.get(node)) {
-            return;
-        }
-        distMap.put(node, elapsed);
-        if (graph.containsKey(node)) {
-            for (int[] pair : graph.get(node)) {
-                dfs(graph, distMap, pair[0], elapsed+pair[1]);
-            }
-        }
-    }
 }
+
+// Approach #1: Depth-First Search
+
+// class Solution {
+//     public int networkDelayTime(int[][] times, int n, int k) {
+//         Map<Integer, List<int[]>> graph = new HashMap<>();
+//         for (int[] edge : times) {
+//             if (!graph.containsKey(edge[0])) {
+//                 graph.put(edge[0], new ArrayList<>());
+//             }
+//             graph.get(edge[0]).add(new int[]{edge[1], edge[2]});
+//         }
+//         for (int node: graph.keySet()) {
+//             Collections.sort(graph.get(node), (a, b) -> a[1] - b[1]);
+//         }
+        
+//         Map<Integer, Integer> distMap = new HashMap<>();
+//         for (int node = 1; node <= n; node++) {
+//             distMap.put(node, Integer.MAX_VALUE);
+//         }
+        
+//         dfs(graph, distMap, k, 0);
+        
+//         int res = 0;
+//         for (int cand : distMap.values()) {
+//             if (cand == Integer.MAX_VALUE) {
+//                 return -1;
+//             }
+//             res = Math.max(res, cand);
+//         }
+//         return res;
+//     }
+    
+//     private void dfs(Map<Integer, List<int[]>> graph, Map<Integer, Integer> distMap, int node, int elapsed) {
+//         if (elapsed >= distMap.get(node)) {
+//             return;
+//         }
+//         distMap.put(node, elapsed);
+//         if (graph.containsKey(node)) {
+//             for (int[] pair : graph.get(node)) {
+//                 dfs(graph, distMap, pair[0], elapsed+pair[1]);
+//             }
+//         }
+//     }
+// }
